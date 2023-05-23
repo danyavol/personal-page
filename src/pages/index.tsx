65609ef39c7b09649b0144100f5a388fdd50939c
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2023-05-23 13:28:15 +0400

    fix: Build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,29 +7,29 @@ import dynamic from 'next/dynamic'
 
 const inter = Inter({ subsets: ['latin'] });
 
-const DynamicProjects = dynamic(() => import('./projects/projects'), { ssr: false });
+const DynamicProjects = dynamic(() => import('../components/projects/projects'), { ssr: false });
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Daniil V. - Personal Page</title>
-        <meta name="description" content="My Personal Page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg"></link>
-      </Head>
-      <div className={inter.className}>
-        <main>
-                <Header />
+    return (
+        <>
+            <Head>
+                <title>Daniil V. - Personal Page</title>
+                <meta name="description" content="My Personal Page" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg"></link>
+            </Head>
+            <div className={inter.className}>
+                <main>
+                    <Header />
 
-                <h3 style={{color: "red", textAlign: "center"}}>Site is in progress... Will be available soon</h3>
-                {/* <About /> */}
-                <DynamicProjects />
-                {/* <Contact /> */}
-        </main>
-        <div className="page-background"></div>
-        <div id="overlay"></div>
-      </div>
-    </>
-  )
+                    <h3 style={{ color: "red", textAlign: "center" }}>Site is in progress... Will be available soon</h3>
+                    {/* <About /> */}
+                    <DynamicProjects />
+                    {/* <Contact /> */}
+                </main>
+                <div className="page-background"></div>
+                <div id="overlay"></div>
+            </div>
+        </>
+    )
 }
