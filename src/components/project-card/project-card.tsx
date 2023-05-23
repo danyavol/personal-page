0f386728e8d8209cfc037b8bf7879f8b10248a00
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2023-05-23 12:38:50 +0400

    feat: Add image gallery

--- src/components/project-card/project-card.tsx
+++ src/components/project-card/project-card.tsx
@@ -1,31 +1,95 @@
 import { Project } from "@/constants/projects-list";
 import styles from "./project-card.module.scss";
 import LinkIcon from "../../../public/images/icons/link.svg";
+import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
+import { useRef, useState } from "react";
+import ReactImageGallery from "react-image-gallery";
+import { createPortal } from 'react-dom';
+import { MouseEventHandler } from "react";
 
 export default function ProjectCard(params: { project: Project, top?: boolean }) {
-    const { title, description, tags, images, previewImg, links } = params.project;
-    return (
-        <div className={`${styles.card} ${params.top ? styles.top : ""}`} tabIndex={0}>
-            <div className={styles.cardPreview}>
-                <img src={previewImg} />
-            </div>
-            <div className={styles.cardDescription}>
-                <h3>{title}</h3>
-                <p className={styles.description}>{description}</p>
+    const imageGalleryRef = useRef<ReactImageGallery>(null);
+    const [isFullScreen, setIsFullscreen] = useState(false);
 
-                <div className={styles.links}>
-                    { links?.map((link, index) => (
-                        <a className={styles.link} href={link.url} target="_blank" key={index}>
-                            <LinkIcon />
-                            {link.label}
-                        </a>
-                    )) }
+    const { title, description, tags, images, previewImg, links } = params.project;
+
+    const images2: ReactImageGalleryItem[] = [
+        {
+          original: '/images/projects/study-buddy-bot/preview.png',
+          thumbnail: '/images/projects/study-buddy-bot/preview.png',
+          description: "some description example"
+        },
+        {
+          original: '/images/projects/money-splitter/Screenshot_20230508-143825.png',
+          thumbnail: '/images/projects/money-splitter/Screenshot_20230508-143825.png',
+        },
+        {
+          original: 'https://picsum.photos/id/1019/1000/600/',
+          thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+    ];
+
+    function enterFullScreen() {
+        imageGalleryRef.current?.fullScreen();
+        // imageGalleryRef.current.
+    }
+
+    function fullScreenChangeHandler(isFullScreen: boolean) {
+        setIsFullscreen(isFullScreen);
+        if (!isFullScreen) {
+            // After exit full screen mode go to first image
+            // so next time user open gallery he is on the first image
+            imageGalleryRef.current?.slideToIndex(0);
+            document.documentElement.classList.remove('image-gallery-open');
+        } else {
+            document.documentElement.classList.add('image-gallery-open');
+        }
+    }
+
+    const imageGalleryClickHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+        if ((e.target as HTMLElement).classList.contains("image-gallery-slide")) {
+            imageGalleryRef.current?.exitFullScreen();
+        }
+    }
+
+    return (
+        <>
+            <div className={`${styles.card} ${params.top ? styles.top : ""}`} tabIndex={0}>
+                <div className={styles.cardPreview}>
+                    <img src={previewImg} onClick={enterFullScreen} />
                 </div>
-                
-                <div className={styles.tags}>
-                    {tags.map((tag, index) => <span className={styles.tag} key={index}>{tag}</span>)}
+                <div className={styles.cardDescription}>
+                    <h3>{title}</h3>
+                    <p className={styles.description}>{description}</p>
+
+                    <div className={styles.links}>
+                        { links?.map((link, index) => (
+                              <a className={styles.link} href={link.url} target="_blank" key={index}>
+                                <LinkIcon />
+                                {link.label}
+                            </a>
+                        )) }
+                    </div>
+                    
+                    <div className={styles.tags}>
+                        {tags.map((tag, index) => <span className={styles.tag} key={index}>{tag}</span>)}
+                    </div>
                 </div>
             </div>
-        </div>
+            {createPortal(
+                (<div className={isFullScreen ? "fullScreen" : ""} onClick={imageGalleryClickHandler}>
+                    <ImageGallery 
+                        items={images2} 
+                        ref={imageGalleryRef} 
+                        onScreenChange={fullScreenChangeHandler} 
+                        showPlayButton={false}
+                        useBrowserFullscreen={false}
+                        showBullets={true}
+                        showThumbnails={false}
+                    ></ImageGallery>
+                </div>),
+                document.querySelector('#overlay')!
+            )}
+        </>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Header from './header/header'
 import About from './about/about'
-import Projects from './projects/projects'
 import Contact from './contact/contact'
+import dynamic from 'next/dynamic'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const DynamicProjects = dynamic(() => import('./projects/projects'), { ssr: false });
 
 export default function Home() {
   return (
@@ -16,15 +18,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg"></link>
       </Head>
-      <main className={inter.className}>
-            <Header />
+      <div className={inter.className}>
+        <main>
+                <Header />
 
-            <h3 style={{color: "red", textAlign: "center"}}>Site is in progress... Will be available soon</h3>
-            {/* <About /> */}
-            <Projects />
-            {/* <Contact /> */}
-      </main>
-      <div className="page-background"></div>
+                <h3 style={{color: "red", textAlign: "center"}}>Site is in progress... Will be available soon</h3>
+                {/* <About /> */}
+                <DynamicProjects />
+                {/* <Contact /> */}
+        </main>
+        <div className="page-background"></div>
+        <div id="overlay"></div>
+      </div>
     </>
   )
 }
