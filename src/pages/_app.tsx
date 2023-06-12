b4189948adfdc147c48e960e8c6439513f07cc24
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2023-06-12 15:46:45 +0200

    feat: Add gTag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
+import { isProduction } from '@/helpers/getEnvironment'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <>
+        {
+            isProduction && <>
+                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-R7ND9RKK8F" />
+                <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-R7ND9RKK8F');
+                    `
+                }} />
+            </>
+        }
+        <Component {...pageProps} />
+    </>
 }
