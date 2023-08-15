6e8c81b382dc3f9415604a01a263aa4db261da69
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2023-08-15 22:39:41 +0200

    fix: Fix hotjar tracking code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Hotjar from '@hotjar/browser';
-
-if (process.env.NODE_ENV == "production"){
-    Hotjar.init(3614844, 6);
-}
 
 export default function Document() {
     return (
